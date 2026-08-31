84f4b9aafdcecb019e0808675d4f6a721a0a2acc
littlenines <nemanja584@hotmail.com>
2026-08-31 15:32:32 +0200

    add login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,9 +1,15 @@
 import { useState, type SubmitEvent } from "react";
+import { Link } from "react-router";
+import Envelope from "@/components/icons/Envelope";
+import ShieldSlash from "@/components/icons/ShieldSlash";
+import AuthLayout from "@/components/AuthLayout";
+import Input from "@/components/Input";
+import Checkbox from "@/components/Checkbox";
+import SubmitButton from "@/components/SubmitButton";
 import api from "@/config/api";
 
 const Login = () => {
-
-const [login, setlogin] = useState({ email: '', password: '', })
+const [login, setLogin] = useState({ email: '', password: '', remember: false })
 
 const submitLogin = async (event: SubmitEvent<HTMLFormElement>) => {
   event.preventDefault();
@@ -11,21 +17,24 @@ const submitLogin = async (event: SubmitEvent<HTMLFormElement>) => {
   const loginFetch = await api.post("auth/login", login)
   console.log(loginFetch)
 }
-return (
-  <main>
-    <section className="login">
-      <div className="login-wrapper">
 
-      <h1>Login your account</h1>
-      <p>Welcome back! Select method to login:</p>
-      <form className="login_form" onSubmit={submitLogin}>
-        <input type="email" placeholder="Email" value={login.email} onChange={(event) => setlogin({ ...login, email: event.target.value })}/>
-        <input type="password" placeholder="password" value={login.password} onChange={(event) => setlogin({ ...login, password: event.target.value })} />
-        <button type="submit">Login</button>
-    </form>
-      </div>
-    </section>
-  </main>
+return (
+     <AuthLayout
+      title="Login to your Account"
+      subtitle="Welcome back! Select method to log in:"
+      onSubmit={submitLogin}
+      footer={<>Don't have an account? <Link to={'/register'}>Create an account</Link></>}
+      illustration={{
+        src: "/two_factor.svg",
+        title: "Connect with any device.",
+        subtitle: "Everything you need is an internet connection.",
+      }}
+    >
+      <Input icon={<Envelope />} type="email" placeholder="Email" value={login.email} onChange={(event) => setLogin({ ...login, email: event.target.value })}/>
+      <Input icon={<ShieldSlash />} type="password" placeholder="Password" value={login.password} onChange={(event) => setLogin({ ...login, password: event.target.value })} />
+      <Checkbox label="Remember me" checked={login.remember} onChange={(event) => setLogin({ ...login, remember: event.target.checked})} />
+      <SubmitButton>Log in</SubmitButton>
+    </AuthLayout>
 )
 }
 
