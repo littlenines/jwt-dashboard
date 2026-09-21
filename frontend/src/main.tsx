6b1e537dcef3b1ae89b2c000a6ea025172236386
littlenines <nemanja584@hotmail.com>
2026-09-21 22:11:50 +0200

    uncomment route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { AuthProvider } from "./context/auth/AuthProvider.tsx";
-// import ProtectedRoute from "./components/ProtectedRoute.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
 import Login from "./pages/Login.tsx";
 import Register from "./pages/Register.tsx";
 import Dashboard from "./pages/Dashboard.tsx";
@@ -18,9 +18,9 @@ createRoot(document.getElementById("root")!).render(
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
-          {/* <Route element={<ProtectedRoute />}>
-          </Route> */}
+          </Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
